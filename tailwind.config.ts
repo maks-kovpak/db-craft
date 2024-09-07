cf3b0895fe1ceda06d1fb01079998e18c225effd
Maksym Kovpak <kovpak.maksym@gmail.com>
2024-09-07 22:43:52 +0300

    chore(deps): add shadcn ui library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,23 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  darkMode: ['class'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      colors: {},
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
+
 export default config;
